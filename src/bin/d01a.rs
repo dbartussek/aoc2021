7c33d2b4dc73d2a6ee2d960fedf384b0c9e55d6f
Dario Bartussek <dario.bartussek@baramundi.com>
2021-12-02 15:10:30 +0100

    d01

---
+++ src/bin/d01a.rs
@@ -0,0 +1,22 @@
+fn main() {
+    let input = std::fs::read_to_string("input/01.txt").unwrap();
+    let numbers = input
+        .lines()
+        .map(|it| it.trim())
+        .filter(|it| !it.is_empty())
+        .map(|it| it.parse::<isize>().unwrap())
+        .collect::<Vec<_>>();
+
+    let mut count = 0usize;
+    let mut it = numbers.iter().copied().peekable();
+
+    while let Some(this) = it.next() {
+        if let Some(next) = it.peek().copied() {
+            if this < next {
+                count += 1;
+            }
+        }
+    }
+
+    println!("{}", count);
+}
