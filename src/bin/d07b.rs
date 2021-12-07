1d756da6cc9eb7e3b4827b127199d5347fc02398
Dario Bartussek <dario.bartussek@baramundi.com>
2021-12-07 09:54:38 +0100

    d07

---
+++ src/bin/d07b.rs
@@ -0,0 +1,28 @@
+#![feature(int_abs_diff)]
+
+use itertools::*;
+
+fn main() {
+    let input: Vec<u32> = std::fs::read_to_string("input/07.txt")
+        .unwrap()
+        .split(',')
+        .map(|s| s.trim().parse().unwrap())
+        .collect();
+    let (min, max) = match input.iter().copied().minmax() {
+        MinMaxResult::MinMax(a, b) => (a, b),
+        _ => panic!(),
+    };
+
+    let min_distance = (min..=max)
+        .map(|destination| {
+            input
+                .iter()
+                .copied()
+                .map(|pos| pos.abs_diff(destination))
+                .flat_map(|distance| 0..=distance)
+                .sum::<u32>()
+        })
+        .min()
+        .unwrap();
+    println!("{}", min_distance);
+}
