07e541e85aa0587784c0944950927cd0a41a38f8
Dario Bartussek <dario.bartussek@baramundi.com>
2021-12-14 18:13:04 +0100

    d13

---
+++ src/bin/d13a.rs
@@ -0,0 +1,54 @@
+use std::collections::HashSet;
+
+fn fold<const N: usize>(
+    points: &HashSet<[u16; N]>,
+    (along, at): (usize, u16),
+) -> HashSet<[u16; N]> {
+    let mut result = HashSet::with_capacity(points.len());
+
+    for mut p in points.iter().copied() {
+        if p[along] == at {
+            continue;
+        }
+        if p[along] > at {
+            p[along] = at - (p[along] - at);
+        }
+
+        result.insert(p);
+    }
+
+    result
+}
+
+fn main() {
+    let input = std::fs::read_to_string("input/13.txt").unwrap();
+
+    let mut points = HashSet::new();
+    let mut folds = Vec::new();
+
+    let fold_prefix = "fold along ";
+    for line in input.lines().map(|s| s.trim()).filter(|s| !s.is_empty()) {
+        if line.starts_with(fold_prefix) {
+            let line = &line[fold_prefix.len()..];
+            let mut parts = line.split('=');
+
+            folds.push((
+                match parts.next().unwrap() {
+                    "x" => 0usize,
+                    "y" => 1usize,
+                    _ => unreachable!(),
+                },
+                parts.next().unwrap().parse::<u16>().unwrap(),
+            ));
+        } else {
+            let mut coords = line.split(',');
+            points.insert([
+                coords.next().unwrap().parse::<u16>().unwrap(),
+                coords.next().unwrap().parse::<u16>().unwrap(),
+            ]);
+        }
+    }
+
+    let after = fold(&points, folds.first().copied().unwrap());
+    println!("{}", after.len());
+}
